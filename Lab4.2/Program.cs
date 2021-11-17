831c10d4344a56074fd1d71dd5ce1b178da5cea3
Andrew Zdornikov <Azdornikov@yandex.ru>
2021-11-17 18:58:31 +0700

    add enter A

--- Lab4.2/Program.cs
+++ Lab4.2/Program.cs
@@ -6,8 +6,8 @@ namespace Lab4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите A:");
+            double a = Convert.ToDouble(Console.ReadLine());
         }
     }
 }
